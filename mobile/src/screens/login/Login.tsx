99650e70135e8b255e1d65ac5207d793a3078ef0
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2021-07-01 15:08:45 -0500

    sign in

--- mobile/src/screens/login/Login.tsx
+++ mobile/src/screens/login/Login.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {
+  ActivityIndicator,
   KeyboardAvoidingView,
   SafeAreaView,
   Text,
@@ -7,14 +8,28 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { Auth } from "aws-amplify";
 import ExitButton from "../../components/buttons/ExitButton";
 import styles from "./styles/LoginStyles";
 
 const Login = ({ navigation }) => {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const toLanding = (): void => navigation.navigate("Landing");
+
+  async function signIn() {
+    setIsLoading(true);
+    try {
+      const user = await Auth.signIn(email, password);
+      setIsLoading(false);
+      console.log("user logged in: ", user);
+    } catch (error) {
+      setIsLoading(false);
+      console.log("error signing in", error);
+    }
+  }
 
   const handleEmail = (email: string): void => setEmail(email);
   const handlePassword = (password: string): void => setPassword(password);
@@ -35,18 +50,23 @@ const Login = ({ navigation }) => {
           value={email}
           onChangeText={handleEmail}
           returnKeyType={"next"}
+          autoCapitalize="none"
         />
         <Text style={styles.inputTitle}>Password</Text>
         <TextInput
           style={styles.registerInput}
           value={password}
           onChangeText={handlePassword}
+          autoCapitalize="none"
+          secureTextEntry={true}
         />
         <View style={styles.btnContainer}>
-          <TouchableOpacity
-            style={[styles.btnPrimary]}
-            onPress={() => console.log("signed in!")}>
-            <Text style={styles.btnTextPrimary}>Login</Text>
+          <TouchableOpacity style={[styles.btnPrimary]} onPress={signIn}>
+            {isLoading ? (
+              <ActivityIndicator color="white" size="small" />
+            ) : (
+              <Text style={styles.btnTextPrimary}>Login</Text>
+            )}
           </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
