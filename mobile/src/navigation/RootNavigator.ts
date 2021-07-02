1275cb33f140d92a2793e37f0ab2cdbf766f0cb8
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2021-07-02 12:48:34 -0500

    fetch session from splash

--- mobile/src/navigation/RootNavigator.ts
+++ mobile/src/navigation/RootNavigator.ts
@@ -2,10 +2,11 @@ import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import AppTabs from "./AppNavigator";
 import AuthNavigator from "./AuthNavigator";
 import Landing from "../screens/landing/Landing";
+import Splash from "../screens/splash/Splash";
 
 const AppContainer = createAppContainer(
   createSwitchNavigator({
-    Landing: Landing,
+    Splash: Splash,
     Auth: AuthNavigator,
     App: AppTabs,
   }),
---
+++ mobile/src/screens/splash/SplashStyles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../styles/colors";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: colors.white,
+  },
+  title: {
+    marginVertical: 20,
+    color: colors.black,
+    fontWeight: "600",
+    fontSize: 24,
+  },
+});
+
+export default styles;
