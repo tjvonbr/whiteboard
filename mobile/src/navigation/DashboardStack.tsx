7287e79aff9654f541b48dcf90ebd5a8291974cd
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2021-07-11 18:17:08 -0500

    added scoring style dropdown

--- mobile/src/navigation/DashboardStack.tsx
+++ mobile/src/navigation/DashboardStack.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import AddWorkout from "../screens/add-workout/AddWorkout";
 import Dashboard from "../screens/dashboard/Dashboard";
+import WorkoutDetails from "../screens/add-workout/WorkoutDetails";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ function DashboardStack() {
         options={{ title: "Add Workout" }}
       />
       <Stack.Screen name="Dashboard" component={Dashboard} />
+      <Stack.Screen name="Details" component={WorkoutDetails} />
     </Stack.Navigator>
   );
 }
---
+++ mobile/src/screens/add-workout/WorkoutDetails.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { FlatList, SafeAreaView, Text } from "react-native";
+import styles from "./AddWorkoutStyles";
+
+const WorkoutDetails = ({ route }) => {
+  const { exercises } = route.params;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Workout Details</Text>
+    </SafeAreaView>
+  );
+};
+
+export default WorkoutDetails;
